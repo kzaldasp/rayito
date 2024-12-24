1ea4054eb8c8be0697d7b3302faabe6a9f11d11e
kzaldasp <kevinaldas290621@gmail.com>
2024-12-23 19:06:20 -0500

    fotos

--- script/letter.js
+++ script/letter.js
@@ -8,9 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
         button.style.display = 'none';
 
         // Mostrar la carta y empezar a escribir
-        const loremText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula turpis id volutpat ultricies. Morbi eget urna aliquam, tincidunt augue non, eleifend turpis. Nullam a dapibus diam. Maecenas sit amet efficitur neque. Donec pretium suscipit lectus, vel suscipit neque. Morbi consequat scelerisque lorem et placerat. Mauris rhoncus, tellus vulputate commodo ornare, libero nulla blandit leo, sed vestibulum est arcu a velit. Vestibulum sed blandit neque. Integer et mauris quis libero dignissim elementum a id quam.
-
-Fusce dolor justo, blandit quis faucibus in, congue sed leo. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed suscipit tortor elit, at varius magna dapibus ut. Mauris luctus est id nisi blandit, at tincidunt dolor dapibus. Nullam imperdiet purus est, in hendrerit dui bibendum a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas blandit est nisi, non cursus lorem euismod eget. Suspendisse quis luctus ipsum.`;
+        const loremText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vehicula turpis id volutpat ultricies. Morbi eget urna aliquam, tincidunt augue non, eleifend turpis. Nullam a dapibus diam. Maecenas sit amet efficitur neque. Donec pretium suscipit lectus, vel suscipit neque. Morbi consequat scelerisque lorem et placerat. Mauris rhoncus, tellus vulputate commodo ornare, libero nulla blandit leo, sed vestibulum est arcu a velit. Vestibulum sed blandit neque. Integer et mauris quis libe`;
 
         let index = 0;
         letterContent.classList.add('visible'); // Mostrar la carta
